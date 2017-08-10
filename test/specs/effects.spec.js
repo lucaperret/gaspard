1dfaf821b4913445120ab7b3ef04666d16ffa6f6
Luca Perret <perret.luca@gmail.com>
2017-08-10 22:25:55 +0200

    test(Effects): Provide methods unit tests

---
+++ test/specs/effects.spec.js
@@ -0,0 +1,47 @@
+import * as effects from 'src/effects'
+/**
+ * @jest-environment jsdom
+ */
+jest.useFakeTimers()
+
+describe('Effects', () => {
+  let element
+  beforeEach(() => {
+    element = document.createElement('div')
+    document.body.appendChild(element)
+  })
+  afterEach(() => {
+    document.body.innerHTML = ''
+  })
+  test('Fade in', () => {
+    element.style.opacity = 1
+    effects.fadeIn(element, 100)
+    expect(element.style.opacity).toBe('0')
+    jest.runAllTimers()
+    expect(Number(element.style.opacity)).toBe(1)
+  })
+  test('RAF fade in', () => {
+    window.requestAnimationFrame = jest.fn(fn => setTimeout(fn, 1))
+    effects.fadeIn(element, 100)
+    jest.runAllTimers()
+    expect(window.requestAnimationFrame).toBeCalled()
+    effects.fadeIn(element)
+    jest.runAllTimers()
+    expect(window.requestAnimationFrame).toBeCalled()
+  })
+  test('Fade out', () => {
+    element.style.opacity = 0
+    effects.fadeOut(element, 100)
+    expect(element.style.opacity).toBe('1')
+    jest.runAllTimers()
+    expect(Number(element.style.opacity)).toBe(0)
+  })
+  test('Hide', () => {
+    effects.hide(element)
+    expect(element.style.display).toBe('none')
+  })
+  test('Show', () => {
+    effects.show(element)
+    expect(element.style.display).toBe('')
+  })
+})
