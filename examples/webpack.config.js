b03de97a8e5b7b247a8ebdc56a9d4ed1a6b05ce5
Luca Perret <perret.luca@gmail.com>
2018-09-26 19:11:17 +0200

    build(npm): bump dependencies version
    babel upgrade and so on

--- examples/webpack.config.js
+++ examples/webpack.config.js
@@ -10,7 +10,7 @@ module.exports = {
       loader: 'babel-loader',
       options: {
         presets: [
-          ['env', {
+          ['@babel/preset-env', {
             modules: false
           }]
         ]
